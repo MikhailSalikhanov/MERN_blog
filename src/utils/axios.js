9cbfd7253ebc789b04c6595fc721fd094bffcc23
MikhailSalikhanov <mikhail.salikhanov@gmail.com>
2022-12-05 06:27:22 +0100

    backend url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000/api'
-    // baseURL: 'https://ms-mern-blog.herokuapp.com/api'
+    // baseURL: 'http://localhost:5000/api'
+    baseURL: 'https://mern-blog-backend-api.onrender.com/api'
 })
 
 instance.interceptors.request.use(config => {
